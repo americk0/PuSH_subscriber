1ad19e04acad50e4ccda9c24042ead25c9b02da3
Ben Watson <americk0@gmail.com>
2016-02-21 17:51:59 -0500

    adds client things

---
+++ db/migrate/20160221223949_create_feeds.rb
@@ -0,0 +1,11 @@
+class CreateFeeds < ActiveRecord::Migration
+  def change
+    create_table :feeds do |t|
+      t.string :title
+      t.text :text
+      t.string :author
+
+      t.timestamps null: false
+    end
+  end
+end
